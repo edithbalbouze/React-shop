a4df9f8e5ac8683a1519f0affb609bf5d08f99c1
Edith <ebalbouze@medine.dev>
2022-07-21 08:51:17 -0400

    Cambios agregados de los retos v12

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         alias: {
             '@components': path.resolve(__dirname, 'src/components/'),
             '@containers': path.resolve(__dirname, 'src/containers/'),
-            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@templates': path.resolve(__dirname, 'src/pages/'),
 			'@routes': path.resolve(__dirname, 'src/routes/'),
             '@style': path.resolve(__dirname, 'src/style/'),
             '@icons': path.resolve(__dirname, 'src/assets/icons/'),
